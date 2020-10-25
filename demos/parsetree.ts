14fabba679fc55fc633956af838f5421049d1e8d
ictrobot <ictrobot@outlook.com>
2020-10-25 20:02:11 +0000

    Improve parse tree demo

--- demos/parsetree.ts
+++ demos/parsetree.ts
@@ -1,4 +1,5 @@
 import {parse} from "../src/parsing/parser";
+import {Location} from "../src/parsing/parsetree";
 
 const testInput = `
 int factorial(int v) {
@@ -6,10 +7,16 @@ int factorial(int v) {
 }
 `.trimStart();
 
-function parseTree(input: string): string {
+function parseTree(input: string, showLocations: boolean = false): string {
     try {
         return JSON.stringify(parse(input), (key, value) => {
-            return key.startsWith("_") || key === "loc" ? undefined : value;
+            if (key.startsWith("_")) return undefined;
+            if (key === "loc" && typeof value === "object") {
+                if (!showLocations) return undefined;
+                const l = value as Location;
+                return `${l.first_line + 1}:${l.first_column} → ${l.last_line + 1}:${l.last_column}`;
+            }
+            return value;
         }, 2);
     } catch (e) {
         return e.toString();
@@ -20,18 +27,24 @@ if (typeof window === 'undefined') {
     console.log(parseTree(testInput));
 } else {
     window.document.write(`
-        <h2>Parse Tree</h2>
+        <h1>c2wasm parse tree</h1>
         <div>
-            <textarea id="textInput" rows="40" style="width: 100%">${testInput}</textarea>
+            <textarea id="textInput" rows="20" style="width: 100%">${testInput}</textarea>
+            <div style="position: absolute; right: 20px">
+                <span>Show locations: </span> 
+                <input type="checkbox" id="showLoc">
+            </div>
             <pre id="output">${parseTree(testInput)}</pre>
         </div>
     `);
 
     const textInput = window.document.getElementById("textInput") as HTMLTextAreaElement;
     const output = window.document.getElementById("output") as HTMLPreElement;
+    const showLoc = window.document.getElementById("showLoc") as HTMLInputElement;
 
     const handler = () => {
-        output.textContent = parseTree(textInput.value);
+        output.textContent = parseTree(textInput.value, showLoc.checked);
     };
     textInput.addEventListener("input", handler);
+    showLoc.addEventListener("change", handler);
 }
