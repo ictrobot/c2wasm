bd6d05a2f784edca460c7319ad1d519b0a108670
ictrobot <ictrobot@outlook.com>
2020-10-25 17:18:35 +0000

    Fix webpack config to ensure correct path is used in generated html

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,14 @@ const TerserPlugin = require('terser-webpack-plugin');
 const demoPath = "./demos/";
 const demoFiles = Object.fromEntries(require("fs")
         .readdirSync(demoPath)
-        .map(fileName => [demoPath + fileName.split(".")[0], demoPath + fileName]));
+        .map(fileName => [fileName.split(".")[0], demoPath + fileName]));
 
 
 module.exports = {
     entry: demoFiles,
     output: {
         filename: "[name].js",
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist/demos/'),
     },
     resolve: {
         extensions: [".ts", ".js"],
