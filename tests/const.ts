7ea1372369faaa3023847378d6f1103270ce849d
ictrobot <ictrobot@outlook.com>
2020-11-02 16:10:54 +0000

    Fix const tests which didn't always return

--- tests/const.ts
+++ tests/const.ts
@@ -3,50 +3,45 @@ import {toIR} from "../src/tree";
 
 test('const int* x', t => {
     t.truthy(toIR(`
-        int main() {
+        void test() {
            int a = 1, b = 2;
            const int* myPointer;
            myPointer = &a;
            myPointer = &b;
-           return 0;
         }
     `));
     t.throws(() => toIR(`
-        int main() {
+        void test() {
            int a = 1;
            const int* myPointer = &a;
            *myPointer = 4;
-           return 0;
         }
     `));
 });
 
 test('int *const x', t => {
     t.truthy(toIR(`
-        int main() {
+        void test() {
            int a = 1, b = 2;
            int *const myPointer = &b;
            *myPointer = a + b;
            *myPointer = 5;
-           return 0;
         }
     `));
     t.throws(() => toIR(`
-        int main() {
+        void test() {
            int a = 1, b = 2;
            int *const myPointer = &b;
            myPointer = &a;
-           return 0;
         }
     `));
 });
 
 test('const int x[]', t => {
     t.throws(() => toIR(`
-        int main() {
+        void test() {
            const int x[] = {1,2,3};
            x[0] = 34;
-           return 0;
         }
     `));
 });
@@ -58,7 +53,7 @@ test("struct with const member", t => {
             int y;
         };
         
-        int test() {
+        void test() {
             struct T myT = {5, 3};
             myT.y = 5;
         }
@@ -70,7 +65,7 @@ test("struct with const member", t => {
             int y;
         };
         
-        int test() {
+        void test() {
             struct T myT = {5, 3};
             myT.x = 5;
         }
@@ -82,7 +77,7 @@ test("struct with const member", t => {
             int y;
         };
         
-        int test() {
+        void test() {
             struct T myT = {5, 3};
             struct T myS = {1, 2};
             myT = myS;
