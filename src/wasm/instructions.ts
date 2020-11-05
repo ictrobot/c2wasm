50efd0d6184f277fcc55a48f11ef42fe603102cd
ictrobot <ictrobot@outlook.com>
2020-11-05 18:18:37 +0000

    WebAssembly memory instructions

--- src/wasm/instructions.ts
+++ src/wasm/instructions.ts
@@ -1,5 +1,5 @@
-import {byte, u32, labelidx, funcidx, typeidx, localidx, globalidx} from "./base_types";
-import {encodeU32} from "./encoding";
+import {byte, u32, labelidx, funcidx, typeidx, localidx, globalidx, i32, i64, f32, f64} from "./base_types";
+import {encodeU32, encodeI32, encodeI64, encodeF32, encodeF64} from "./encoding";
 import {ValueType} from "./wtypes";
 
 export type Instruction = byte[] & {__type_instr__: void};
@@ -40,15 +40,74 @@ export const Instructions = {
         get: indexArg<localidx>(0x20),
         set: indexArg<localidx>(0x21),
         tee: indexArg<localidx>(0x22),
-    },
+    } as const,
     global: {
         get: indexArg<globalidx>(0x23),
         set: indexArg<globalidx>(0x24),
-    }
+    } as const,
 
 
     // memory instructions
-    // ...
+    memory: {
+        size: zeroArgs(0x3F, 0x00),
+        grow: zeroArgs(0x40, 0x00)
+    } as const,
+
+
+    i32: {
+        load: memArg(0x28),
+        load8_s: memArg(0x2C),
+        load8_u: memArg(0x2D),
+        load16_s: memArg(0x2E),
+        load16_u: memArg(0x2F),
+        store: memArg(0x36),
+        store8: memArg(0x3A),
+        store16: memArg(0x3B),
+
+
+        const(x: i32): Instruction {
+            return [0x41 as byte, ...encodeI32(x)] as Instruction;
+        }
+    } as const,
+
+    i64: {
+        load: memArg(0x29),
+        load8_s: memArg(0x30),
+        load8_u: memArg(0x31),
+        load16_s: memArg(0x32),
+        load16_u: memArg(0x33),
+        load32_s: memArg(0x34),
+        load32_u: memArg(0x35),
+        store: memArg(0x37),
+        store8: memArg(0x3C),
+        store16: memArg(0x3D),
+        store32: memArg(0x3E),
+
+
+        const(x: i64): Instruction {
+            return [0x42 as byte, ...encodeI64(x)] as Instruction;
+        }
+    } as const,
+
+    f32: {
+        load: memArg(0x2A),
+        store: memArg(0x38),
+
+
+        const(x: f32): Instruction {
+            return [0x43 as byte, ...encodeF32(x)] as Instruction;
+        }
+    } as const,
+    f64: {
+        load: memArg(0x2B),
+        store: memArg(0x39),
+
+
+        const(x: f64): Instruction {
+            return [0x44 as byte, ...encodeF64(x)] as Instruction;
+        }
+    } as const
+
 } as const;
 
 
@@ -57,9 +116,9 @@ function encodeBlockType(t: ValueType | undefined): byte[] {
     return [t];
 }
 
-function zeroArgs(opcode: number): () => Instruction {
+function zeroArgs(opcode: number, ...extra: number[]): () => Instruction {
     // always return the same instance
-    const instr = [opcode] as Instruction;
+    const instr = [opcode, ...extra] as Instruction;
     return () => instr;
 }
 
@@ -74,4 +133,8 @@ function encodeIndex<T extends u32>(idx: index<T>): byte[] {
 
 function indexArg<T extends u32>(opcode: number): (x: index<T>) => Instruction {
     return (i) => [opcode as byte, ...encodeIndex(i)] as Instruction;
+}
+
+function memArg(opcode: number): (align: u32, offset: u32) => Instruction {
+    return (align, offset) => [opcode as byte, ...encodeU32(align), ...encodeU32(offset)] as Instruction;
 }
