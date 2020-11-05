b5601248a1691cbb35aee441145d71668cccab09
ictrobot <ictrobot@outlook.com>
2020-11-05 18:48:42 +0000

    Remaining instructions

--- src/wasm/instructions.ts
+++ src/wasm/instructions.ts
@@ -67,7 +67,48 @@ export const Instructions = {
 
         const(x: i32): Instruction {
             return [0x41 as byte, ...encodeI32(x)] as Instruction;
-        }
+        },
+
+        eqz: zeroArgs(0x45),
+        eq: zeroArgs(0x46),
+        ne: zeroArgs(0x47),
+        lt_s: zeroArgs(0x48),
+        lt_u: zeroArgs(0x49),
+        gt_s: zeroArgs(0x4A),
+        gt_u: zeroArgs(0x4B),
+        le_s: zeroArgs(0x4C),
+        le_u: zeroArgs(0x4D),
+        ge_s: zeroArgs(0x4E),
+        ge_u: zeroArgs(0x4F),
+
+        clz: zeroArgs(0x67),
+        ctz: zeroArgs(0x68),
+        popcnt: zeroArgs(0x69),
+        add: zeroArgs(0x6A),
+        sub: zeroArgs(0x6B),
+        mul: zeroArgs(0x6C),
+        div_s: zeroArgs(0x6D),
+        div_u: zeroArgs(0x6E),
+        rem_s: zeroArgs(0x6F),
+        rem_u: zeroArgs(0x70),
+        and: zeroArgs(0x71),
+        or: zeroArgs(0x72),
+        xor: zeroArgs(0x73),
+        shl: zeroArgs(0x74),
+        shr_s: zeroArgs(0x75),
+        shr_u: zeroArgs(0x76),
+        rotl: zeroArgs(0x77),
+        rotr: zeroArgs(0x78),
+
+        wrap_i64: zeroArgs(0xA7),
+        trunc_f32_s: zeroArgs(0xA8),
+        trunc_f32_u: zeroArgs(0xA9),
+        trunc_f64_s: zeroArgs(0xAA),
+        trunc_f64_u: zeroArgs(0xAB),
+
+        reinterpret_f32: zeroArgs(0xBC),
+        extend8_s: zeroArgs(0xC0),
+        extend16_s: zeroArgs(0xC1),
     } as const,
 
     i64: {
@@ -86,7 +127,50 @@ export const Instructions = {
 
         const(x: i64): Instruction {
             return [0x42 as byte, ...encodeI64(x)] as Instruction;
-        }
+        },
+
+        eqz: zeroArgs(0x50),
+        eq: zeroArgs(0x51),
+        ne: zeroArgs(0x52),
+        lt_s: zeroArgs(0x53),
+        lt_u: zeroArgs(0x54),
+        gt_s: zeroArgs(0x55),
+        gt_u: zeroArgs(0x56),
+        le_s: zeroArgs(0x57),
+        le_u: zeroArgs(0x58),
+        ge_s: zeroArgs(0x59),
+        ge_u: zeroArgs(0x5A),
+
+        clz: zeroArgs(0x79),
+        ctz: zeroArgs(0x7A),
+        popcnt: zeroArgs(0x7B),
+        add: zeroArgs(0x7C),
+        sub: zeroArgs(0x7D),
+        mul: zeroArgs(0x7E),
+        div_s: zeroArgs(0x7F),
+        div_u: zeroArgs(0x80),
+        rem_s: zeroArgs(0x81),
+        rem_u: zeroArgs(0x82),
+        and: zeroArgs(0x83),
+        or: zeroArgs(0x84),
+        xor: zeroArgs(0x85),
+        shl: zeroArgs(0x86),
+        shr_s: zeroArgs(0x87),
+        shr_u: zeroArgs(0x88),
+        rotl: zeroArgs(0x89),
+        rotr: zeroArgs(0x8A),
+
+        extend_i32_s: zeroArgs(0xAC),
+        extend_i32_u: zeroArgs(0xAD),
+        trunc_f32_s: zeroArgs(0xAE),
+        trunc_f32_u: zeroArgs(0xAF),
+        trunc_f64_s: zeroArgs(0xB0),
+        trunc_f64_u: zeroArgs(0xB1),
+
+        reinterpret_f64: zeroArgs(0xBD),
+        extend8_s: zeroArgs(0xC2),
+        extend16_s: zeroArgs(0xC3),
+        extend32_s: zeroArgs(0xC4),
     } as const,
 
     f32: {
@@ -96,8 +180,39 @@ export const Instructions = {
 
         const(x: f32): Instruction {
             return [0x43 as byte, ...encodeF32(x)] as Instruction;
-        }
+        },
+
+        eq: zeroArgs(0x5B),
+        ne: zeroArgs(0x5C),
+        lt: zeroArgs(0x5D),
+        gt: zeroArgs(0x5E),
+        le: zeroArgs(0x5F),
+        ge: zeroArgs(0x60),
+
+        abs: zeroArgs(0x8B),
+        neg: zeroArgs(0x8C),
+        ceil: zeroArgs(0x8D),
+        floor: zeroArgs(0x8E),
+        trunc: zeroArgs(0x8F),
+        nearest: zeroArgs(0x90),
+        sqrt: zeroArgs(0x91),
+        add: zeroArgs(0x92),
+        sub: zeroArgs(0x93),
+        mul: zeroArgs(0x94),
+        div: zeroArgs(0x95),
+        min: zeroArgs(0x96),
+        max: zeroArgs(0x97),
+        copysign: zeroArgs(0x98),
+
+        convert_i32_s: zeroArgs(0xB2),
+        convert_i32_u: zeroArgs(0xB3),
+        convert_i64_s: zeroArgs(0xB4),
+        convert_i64_u: zeroArgs(0xB5),
+        demote_f64: zeroArgs(0xB6),
+
+        reinterpret_i32: zeroArgs(0xBE),
     } as const,
+
     f64: {
         load: memArg(0x2B),
         store: memArg(0x39),
@@ -105,7 +220,37 @@ export const Instructions = {
 
         const(x: f64): Instruction {
             return [0x44 as byte, ...encodeF64(x)] as Instruction;
-        }
+        },
+
+        eq: zeroArgs(0x61),
+        ne: zeroArgs(0x62),
+        lt: zeroArgs(0x63),
+        gt: zeroArgs(0x64),
+        le: zeroArgs(0x65),
+        ge: zeroArgs(0x66),
+
+        abs: zeroArgs(0x99),
+        neg: zeroArgs(0x9A),
+        ceil: zeroArgs(0x9B),
+        floor: zeroArgs(0x9C),
+        trunc: zeroArgs(0x9D),
+        nearest: zeroArgs(0x9E),
+        sqrt: zeroArgs(0x9F),
+        add: zeroArgs(0xA0),
+        sub: zeroArgs(0xA1),
+        mul: zeroArgs(0xA2),
+        div: zeroArgs(0xA3),
+        min: zeroArgs(0xA4),
+        max: zeroArgs(0xA5),
+        copysign: zeroArgs(0xA6),
+
+        convert_i32_s: zeroArgs(0xB7),
+        convert_i32_u: zeroArgs(0xB8),
+        convert_i64_s: zeroArgs(0xB9),
+        convert_i64_u: zeroArgs(0xBA),
+        promote_f32: zeroArgs(0xBB),
+
+        reinterpret_i64: zeroArgs(0xBF),
     } as const
 
 } as const;
