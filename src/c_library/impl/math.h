06981ea8ead56debc7978759f504ca1698269c20
ictrobot <ictrobot@outlook.com>
2020-11-21 21:09:30 +0000

    Math library stubs

---
+++ src/c_library/impl/math.h
@@ -0,0 +1,36 @@
+#pragma once
+
+// c89 with wasm instructions
+double sqrt(double);
+double ceil(double);
+double fabs(double);
+double floor(double);
+
+// c99 with wasm instructions
+double fmax(double, double);
+double fmin(double, double);
+double trunc(double);
+double copysign(double, double);
+double round(double); // nearest
+
+//double acos(double);
+//double asin(double);
+//double atan(double);
+//double atan2(double, double);
+//double cos(double);
+//double sin(double);
+//double tan(double);
+//
+//double cosh(double);
+//double sinh(double);
+//double tanh(double);
+//
+//double exp(double);
+//double ldexp(double, int);
+//double frexp(double, int *);
+//double log(double);
+//double log10(double);
+//double modf(double, double *);
+//
+//double pow(double, double);
+//double fmod(double, double);
---
+++ src/c_library/impl/stdlib/math.c
@@ -0,0 +1,40 @@
+#include <math.h>
+#include <wasm/f64.h>
+
+// c89 simple wasm instructions
+double sqrt(double d) {
+    return f64_sqrt(d);
+}
+
+double ceil(double d) {
+    return f64_ceil(d);
+}
+
+double fabs(double d) {
+    return f64_abs(d);
+}
+
+double floor(double d) {
+    return f64_floor(d);
+}
+
+// c99 simple wasm instructions
+double fmax(double d1, double d2) {
+    return f64_max(d1, d2);
+}
+
+double fmin(double d1, double d2) {
+    return f64_min(d1, d2);
+}
+
+double trunc(double d) {
+    return f64_trunc(d);
+}
+
+double copysign(double d1, double d2) {
+    return f64_copysign(d1, d2);
+}
+
+double round(double d) {
+    return f64_nearest(d);
+}
