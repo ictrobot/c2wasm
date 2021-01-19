2adeb07f0496311e00f10949f922cbd403d27fbb
ictrobot <ictrobot@outlook.com>
2021-01-19 16:45:00 +0000

    Speed up reaching definitions analysis by storing the set of definitions as bits on a bigint

--- src/optimization/flow/data_flow.ts
+++ src/optimization/flow/data_flow.ts
@@ -5,31 +5,38 @@ export type Definition = {
     readonly local: bigint,
     possibleUses: InstrFlow[], // instructions which reference this definition
     definiteUses: InstrFlow[], // instructions which reference this definition and no other possible definition
+    bit: bigint,
 } & ({type: "arg"} | {type: "local.set" | "local.tee", flow: InstrFlow});
 
 export function reachingDefinitions(expr: WExpression): Definition[] {
     const {all} = simplifiedControlFlow(expr, instr => instr.name.startsWith("local."));
     if (all.length === 0) return [];
 
-    const reachingDefs = new Map<InstrFlow, Set<Definition>>();
+    const reachingDefs = new Map<InstrFlow, bigint>(); // each bit represents
     const flowDefMap = new Map<InstrFlow, Definition>();
     const allDefinitions: Definition[] = [];
     const queue = new Set<InstrFlow>();
 
+    const numLocals = expr.builder.args.length + expr.builder.locals.length;
+    // mask containing the bits for each local allowing quick killing of all a locals definitions
+    const defMask: bigint[] = Array(numLocals).fill(0n);
+
     // entry definitions are the function parameters
-    const entryDefinitions = new Set<Definition>();
+    let entryDefinitions = 0n;
     for (let i = 0n; i < expr.builder.args.length; i++) {
-        const d: Definition = {local: i, type: "arg", possibleUses: [], definiteUses: []};
-        entryDefinitions.add(d);
+        const d: Definition = {local: i, type: "arg", possibleUses: [], definiteUses: [], bit: 1n << BigInt(allDefinitions.length)};
+        entryDefinitions |= d.bit;
+        defMask[Number(i)] |= d.bit;
         allDefinitions.push(d);
     }
 
     for (const f of all) {
-        reachingDefs.set(f, new Set());
+        reachingDefs.set(f, 0n);
         queue.add(f);
 
         if (f.instr.type === "index" && (f.instr.name === "local.set" || f.instr.name === "local.tee")) {
-            const d: Definition = {possibleUses: [], definiteUses: [], local: f.instr.immediate.value, type: f.instr.name, flow: f};
+            const d: Definition = {possibleUses: [], definiteUses: [], local: f.instr.immediate.value, type: f.instr.name, flow: f, bit: 1n << BigInt(allDefinitions.length)};
+            defMask[Number(d.local)] |= d.bit;
             flowDefMap.set(f, d);
             allDefinitions.push(d);
         }
@@ -41,7 +48,7 @@ export function reachingDefinitions(expr: WExpression): Definition[] {
         queue.delete(flow);
         // OUT[n] = GEN[n] Union (IN[n] -KILL[n]);
 
-        const S = new Set<Definition>();
+        let S = 0n;
         for (const p of flow.flowPrevious) {
             let pSet;
             if (p.type === "instr") {
@@ -49,23 +56,17 @@ export function reachingDefinitions(expr: WExpression): Definition[] {
             } else if (p.type === "entry") {
                 pSet = entryDefinitions;
             }
-            if (pSet) {
-                for (const value of pSet) S.add(value);
-            }
+            S |= pSet ?? 0n;
         } // S = IN[n]
 
         const flowDef = flowDefMap.get(flow);
         if (flowDef) {
-            for (const existing of S) {
-                if (flowDef.local === existing.local) {
-                    S.delete(existing);
-                }
-            } // S = IN[n] - KILL[n]
+            S &= ~defMask[Number(flowDef.local)]; // S = IN[n] - KILL[n]
 
-            S.add(flowDef);
+            S |= flowDef.bit;
         } // S = OUT[n]
 
-        if (!setEquals(S, reachingDefs.get(flow))) {
+        if (S !== reachingDefs.get(flow)) {
             reachingDefs.set(flow, S);
 
             for (const n of flow.flowNext) {
@@ -78,7 +79,7 @@ export function reachingDefinitions(expr: WExpression): Definition[] {
     for (const [flow, defs] of reachingDefs.entries()) {
         if (flow.instr.type !== "index" || flow.instr.name !== "local.get") continue;
         const local = flow.instr.immediate.value;
-        const localDefs = [...defs].filter(x => x.local === local);
+        const localDefs = allDefinitions.filter(d => (d.bit & defs) && d.local === local);
 
         if (localDefs.length === 1) {
             localDefs[0].definiteUses.push(flow);
@@ -87,12 +88,4 @@ export function reachingDefinitions(expr: WExpression): Definition[] {
     }
 
     return allDefinitions;
-}
-
-function setEquals<T>(a: Set<T>, b: Set<T> | undefined): boolean {
-    if (!b || a.size !== b.size) return false;
-    for (const x of a) {
-        if (!b.has(x)) return false;
-    }
-    return true;
 }
