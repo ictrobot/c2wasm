56696a3a1a59021094dbcc6881b89a1c99a81437
ictrobot <ictrobot@outlook.com>
2021-02-04 19:01:25 +0000

    Check coremark compiles & runs automatically as a unit test

---
+++ tests/benchmarks.ts
@@ -0,0 +1,32 @@
+import test from "ava";
+import fs from "fs";
+import path from "path";
+import type {BenchmarkBase} from "./benchmark/base";
+
+// Check the benchmarks run successfully
+// As the benchmarks expect to be run in the tests folder (and not compiled and ran from build/), we use
+// ts-node/register and manually important them from the benchmark directory
+
+// find project directory
+const benchmarkDir = (() => {
+    let projectDir = __dirname;
+    while (!fs.existsSync(path.join(projectDir, "package.json"))) {
+        const newDir = path.join(projectDir, "..");
+        if (newDir !== projectDir) {
+            projectDir = newDir;
+        } else {
+            throw new Error("Couldn't find project root dir");
+        }
+    }
+    return path.join(projectDir, "tests", "benchmark");
+})();
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+require('ts-node').register({});
+const coremark = require(path.join(benchmarkDir, "coremark")).coremark as BenchmarkBase;
+
+test("coremark", async t => {
+    const output = await coremark.c2wasmRun();
+    t.log(output);
+    t.truthy(coremark.getScore(output));
+});
