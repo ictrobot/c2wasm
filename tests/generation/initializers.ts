37fdd590acb14fc5417770b7f4b1a995f025f33b
ictrobot <ictrobot@outlook.com>
2021-05-06 15:36:00 +0100

    Extra initializer tests

--- tests/generation/initializers.ts
+++ tests/generation/initializers.ts
@@ -45,3 +45,79 @@ void main() { puts(myStrings.x); }
     main();
     t.deepEqual(output, "aaaa\n");
 });
+
+test("every type", async t => {
+    const {main, __mem: mem} = await compile(`
+#include <stdbool.h>
+
+struct demo {
+  char string[20];
+  unsigned char a;
+  signed char b;
+  unsigned short c;
+  signed short d;
+  unsigned int e;
+  signed int f;
+  unsigned long g;
+  signed long h;
+  float i;
+  double j;
+  bool k;
+} var = {"Hello World", 123, -5, 2344, 2134, 9898989, -1234323, 1152921504606846976, -1152921504606846976, 1.5f, 0.1f, true};
+
+int main() {
+  return (int) &var;
+}
+    `).execute({}) as {
+        main: () => number,
+        __mem: WebAssembly.Memory
+    };
+
+    const addr = main();
+    const u8mem = new Uint8Array(mem.buffer);
+
+    const expected = [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 123, 251, 40, 9, 86, 8, 0, 0, 237, 11, 151, 0, 109, 42, 237, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 240, 0, 0, 192, 63, 0, 0, 0, 0, 154, 153, 153, 153, 153, 153, 185, 63, 1];
+    const actual = [];
+    for (let i = 0; i < expected.length; i++) {
+        actual.push(u8mem[addr + i]);
+    }
+
+    t.deepEqual(actual, expected);
+});
+
+test("union initializer", async t => {
+    const {main, getDouble, __mem: mem} = await compile(`
+#include <stdbool.h>
+
+union demo {
+    long l;
+    double d;
+} var = {1152921504606846976};
+
+int main() {
+  return (int) &var;
+}
+
+double getDouble() {
+    return var.d;
+}
+    `).execute({}) as {
+        main: () => number,
+        getDouble: () => number
+        __mem: WebAssembly.Memory
+    };
+
+    const addr = main();
+    const u8mem = new Uint8Array(mem.buffer);
+
+    const expected = [0, 0, 0, 0, 0, 0, 0, 16];
+    const actual = [];
+    for (let i = 0; i < expected.length; i++) {
+        actual.push(u8mem[addr + i]);
+    }
+
+    t.deepEqual(actual, expected);
+
+    // check that getDouble() == 1.288229753919427e-231 within epsilon
+    t.assert(Math.abs(getDouble() - 1.288229753919427e-231) < 1e-100);
+});
